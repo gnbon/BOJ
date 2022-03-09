5022127a8628f1041236bd836dd4239376df2395
gnbon <borngunk7061@naver.com>
2022-03-09 23:35:09 +0900

    Add: recursive/11729

---
+++ recursive/11729.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void hanoi(int a, int b, int n) {
+    if (n == 1) {
+        cout << a << " " << b << "\n";
+        return;
+    }
+
+    hanoi(a, 6-a-b, n - 1);
+    cout << a << " " << b << "\n"; 
+    hanoi(6-a-b, b, n - 1);
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll n;
+    cin >> n;
+    cout << (1<<n) - 1 << "\n";
+    hanoi(1, 3, n);
+}
