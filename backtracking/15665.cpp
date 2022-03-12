7cb1bca80e023e16dd19a982a55d39b42ad60234
gnbon <borngunk7061@naver.com>
2022-03-12 16:18:42 +0900

    Add: backtracking/15665

---
+++ backtracking/15665.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int arr[10];
+int num[10];
+
+void solve(int lv)
+{
+    if (lv == m) {
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << " ";
+        cout << "\n";
+        return;
+    }
+
+    int tmp = 0;
+    for (int i = 0; i < n; i++) {
+        if (tmp == num[i]) continue;
+        arr[lv] = num[i];
+        tmp = arr[lv];
+        solve(lv+1);
+    }
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    for (int i = 0 ; i < n; i++)
+        cin >> num[i];
+    sort(num, num+n);
+    solve(0);
+}
